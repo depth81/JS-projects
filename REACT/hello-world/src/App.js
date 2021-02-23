053f4ca447f97d9caf11e5d38c63d9cde488ae30
depth81 <petv1981@gmail.com>
2021-02-22 23:44:03 -0500

    update react

--- REACT/hello-world/src/App.js
+++ REACT/hello-world/src/App.js
@@ -3,15 +3,41 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import React, {Component} from 'react'
+import Message from './components/Message'
+import Counter from './components/Counter'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Greet />
-      <Welcome /> */}
-      <Hello />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">      
+//       <Message />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        {/* <Counter /> */}
+        {/* <Message /> */}
+        {/* <Greet name="Bruce" heroName="Batman">
+          <p>This is children props</p>  
+        </Greet>
+        <Greet name="Clark" heroName="Superman">
+          <button>CLICK!</button>
+        </Greet>
+        <Greet name="Diana" heroName="WonderWoman"/>
+        <Welcome name="Bruce" heroName="Batman"></Welcome>
+        <Welcome name="Clark" heroName="Superman"></Welcome>
+        <Welcome name="Diana" heroName="WonderWoman"></Welcome> */}
+        {/* <Welcome /> */}
+        {/* <Hello /> */}
+        <Greet name="Diana" heroName="WonderWoman"/>
+        <Welcome name="Bruce" heroName="Batman"></Welcome>
+      </div>
+    )
+  }
 }
 
 export default App;
--- REACT/hello-world/src/components/Greet.js
+++ REACT/hello-world/src/components/Greet.js
@@ -4,6 +4,26 @@ import React from 'react'
     return <h1>Hello PAULO ENRIQUE!!!</h1>
 } */
 
-const Greet = () => <h1>Hello PAULO ENRIQUE!!!</h1>
+// const Greet = ({name , heroName}) => {
+//     return (
+//         <div>
+//             <h1>
+//                 Hello {name} a.k.a. {heroName}!!!
+//             </h1>
+//         </div>
+//     )
+// }
+
+const Greet = props => {
+    const {name, heroName} = props
+    return (
+        <div>
+            <h1>
+                Hello {name} a.k.a. {heroName}!!!
+            </h1>
+        </div>
+    )
+}
+
 
 export default Greet
