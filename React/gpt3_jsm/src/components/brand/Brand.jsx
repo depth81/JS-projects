c291c824255ed0a4c48aeacb7a832458d91fe3b9
Paulo Enrique <42650880+egosumscientia@users.noreply.github.com>
2023-10-23 20:55:16 -0500

    new project gpt3
    https://www.youtube.com/watch?v=F627pKNUCVQ&list=WL&index=2&t=484s

---
+++ React/gpt3_jsm/src/components/brand/Brand.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CTA.css';
+
+const Brand = () => {
+  return (
+    <div>
+        Brand
+    </div>
+  )
+}
+
+export default Brand
---
+++ React/gpt3_jsm/src/components/cta/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './article.css';
+
+const CTA = () => {
+  return (
+    <div>
+        CTA
+    </div>
+  )
+}
+
+export default CTA
